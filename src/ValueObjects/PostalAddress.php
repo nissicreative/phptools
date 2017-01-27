367b46cc1a7d6425d10761fadbb1a618f9f400bb
Michael Folsom <mike@nissicreative.com>
2017-01-26 20:51:01 -0600

    Added properties to PostalAddress

--- src/ValueObjects/PostalAddress.php
+++ src/ValueObjects/PostalAddress.php
@@ -1,9 +1,13 @@
 <?php
 namespace Nissi\ValueObjects;
 
+use Nissi\Traits\FormatsName;
+use Nissi\Traits\FormatsPhone;
+use Nissi\Traits\FormatsAddress;
+
 class PostalAddress extends AbstractValueObject
 {
-    use \Nissi\Traits\FormatsAddress;
+    use FormatsAddress, FormatsPhone, FormatsName;
 
     protected $first_name;
     protected $last_name;
@@ -12,7 +16,14 @@ class PostalAddress extends AbstractValueObject
     protected $company;
     protected $organization;
     protected $street;
+    protected $street1;
+    protected $street2;
+    protected $street_1;
+    protected $street_2;
     protected $address;
+    protected $address1;
+    protected $address2;
+    protected $address3;
     protected $address_1;
     protected $address_2;
     protected $address_3;
@@ -21,10 +32,13 @@ class PostalAddress extends AbstractValueObject
     protected $state;
     protected $region;
     protected $zip;
+    protected $zip4;
     protected $postal_code;
     protected $country;
     protected $lat;
     protected $lng;
+    protected $phone;
+    protected $email;
 
     /*
      * By default, use mailing label when casting to string.
