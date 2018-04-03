fcd08006e4aeea709426d50ef8e67a9cdc319748
Michael Folsom <mike@nissicreative.com>
2018-04-02 20:34:49 -0500

    Add HasImage Trait

---
+++ src/Traits/HasImage.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Nissi\Traits;
+
+use Illuminate\Http\UploadedFile;
+
+trait HasImage
+{
+    /**
+     * The URL of the cached image.
+     */
+    public function imgSrc($size = 'full')
+    {
+        $attribute = $this->getImagePathAttributeName();
+
+        return sprintf(
+            '/%s/%s/%s',
+            config('imagecache.route', 'images/cache'),
+            $size,
+            basename($this->$attribute)
+        );
+    }
+
+    /**
+     * Move uploaded image file to appropriate path and update the object.
+     */
+    public function storeImage(UploadedFile $file)
+    {
+        $attribute = $this->getImagePathAttributeName();
+        $path = $file->store($this->getImageDirectory());
+
+        $this->update([
+            $attribute => $path,
+            'mime_type' => $file->getMimeType(),
+            'file_size' => $file->getSize(),
+            'original_name' => $file->getClientOriginalName(),
+        ]);
+    }
+
+    /**
+     * The name of the directory in which to store the image.
+     */
+    public function getImageDirectory()
+    {
+        return $this->imageDirectory ?: str_slug($this->getTable());
+    }
+
+    /**
+     * The name of the attribute used to store the image path.
+     */
+    public function getImagePathAttributeName()
+    {
+        return $this->imagePathAttributeName ?: 'image_path';
+    }
+
+}
